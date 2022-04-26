5c959f71d13a6634d0fc5a4a9b16f103f10120ad
pge1414 <101663790+pge1414@users.noreply.github.com>
2022-04-26 17:35:02 +0200

    as

---
+++ Funktionen.py
@@ -0,0 +1,21 @@
+
+# Bibt True zurück, falls Zahl gerade ist ansonsten false
+# def gerade(zahl: int) -> bool:
+#     if zahl % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+# print(gerade(5))
+
+# Gibt die größere der beiden Zahlen zurück, falls Zahl gerade ist ansonsten
+
+# def größere(zahl1: int, zahl2: int) -> int:
+#     if zahl1 < zahl2:
+#         return zahl2
+#     if zahl1 > zahl2:
+#         return zahl1
+#     else:
+#         print("Zahlen sind gleich groß")
+#         return
+# print(größere(2,4))
--- for Schleifen.py
+++ for Schleifen.py
@@ -86,14 +86,63 @@
 #         print(huener_beine, kanienchen_beine)
 #         break
 
-beine = 94
-köpfe = 35
-huener_beine = 2
-kanienchen_beine = 4
-for h in range(36):
-    for k in range(36):
-        pa = h*huener_beine /2
-        pa2 = k*kanienchen_beine /4
-        ergebnis = h*huener_beine + k*kanienchen_beine
-        if ergebnis == beine and h +k == köpfe:
-            print(pa, pa2)
+# beine = 94
+# köpfe = 35
+# huener_beine = 2
+# kanienchen_beine = 4
+# for h in range(36):
+#     for k in range(36):
+#         pa = h*huener_beine /2
+#         pa2 = k*kanienchen_beine /4
+#         ergebnis = h*huener_beine + k*kanienchen_beine
+#         if ergebnis == beine and h +k == köpfe:
+#             print(pa, pa2)
+
+# from random import randint
+# beine = 94
+# köpfe = 35
+# huener_beine = 2
+# kanienchen_beine = 4
+# b = True
+# while b == True:
+#     x = randint(1, 35)
+#     y = randint(1, 35)
+#     erg_h = x*huener_beine + y*kanienchen_beine
+#     if erg_h == 94 and x +y == 35:
+#         print(x, y)
+#         b = False
+
+# beine = 114
+# köpfe = 26
+# huener_beine = 2
+# kanienchen_beine = 4
+# spinnen_beine = 8
+# hüner_schwanz = 1
+# kanienchen_schwanz = 1
+# for h in range(26):
+#     for k in range(26):
+#         for s in range(26):
+#             pa = h*huener_beine /2
+#             pa2 = k*kanienchen_beine /4
+#             pa3 = s*spinnen_beine/8
+#             ergebnis = h*huener_beine + k*kanienchen_beine + s*spinnen_beine
+#             schwänze_anzahl = h*hüner_schwanz + k*kanienchen_schwanz
+#             if ergebnis == beine and h +k +s == köpfe and schwänze_anzahl == 19:
+#                 print(pa, pa2, pa3)
+# p = 0
+# for x in range(101):
+#     p = x + p
+# print(p)
+#
+# p = 0
+# for x in range(-50, 151):
+#     if x % 2 == 0:
+#         p = p + x 
+# print(p)
+
+# p = 0
+# for x in range(0, 500):
+#     if x % 3 == 0:
+#         if x % 7 ==0:
+#             p +=1
+# print(p)
