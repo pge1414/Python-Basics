1707cfec1a6e378e055ff579f6615b90cc6bc2e4
pge1414 <101663790+pge1414@users.noreply.github.com>
2022-05-31 19:03:16 +0200

    ewrw

--- Classes.py
+++ Classes.py
@@ -1,3 +1,4 @@
+import random
 class Tier:
     #Konstruktor
     def __init__(self,tierart: str, nahrung: list, fressfeinde: list, gewicht: float, größe: int, farbe: str):
@@ -21,16 +22,78 @@ class Tier:
 
     #das tier nimmt jeden tag um 1% an gewicht und 0.5% an größe zu    
     def wachsen(self, tage: int) -> None:
-        self.gewicht *= 1.01 ** tage
-        self.größe *= 1.005 ** tage
-        return self.gewicht, self.größe
+        for i in range(tage):
+            self.gewicht += self.gewicht * 0.01
+            self.größe += self.größe * 0.005
+
+
+    def junges(self) -> "Tier":
+        gewicht_junges = round(random.uniform(self.gewicht * 0.05, self.gewicht * 0.15), 2)
+        größe_junges = int(random.uniform(self.größe * 0.25, self.größe * 0.35))
+        nahrung_junges = self.nahrung
+        tierart_junges = self.tierart
+        farbe_junges = self.farbe
+        fressfeinde_junges = self.fressfeinde
+        junges = Tier(tierart_junges, nahrung_junges, fressfeinde_junges, gewicht_junges, größe_junges, farbe_junges)
+        return junges
+
+class Safaripark:
+    def __init__(self, name: str, tiere: list, tourpreis: float):
+        self.name = name
+        self.tiere = tiere
+        self.tourpreis = tourpreis
+    #gibt die anzahl an tieren zurück
+    def anzahl_tiere(self) -> int:
+        return len(self.tiere)
+
+    #gibt den preis pro einem tier zurück
+    def preis_pro_tier(self) ->float:
+        return self.tourpreis / self.anzahl_tiere
+
+    #nimmt tier in den Safaripark auf
+    def aufnehmen(self, tier: Tier) -> None:
+        self.tiere.append(tier)
+
+    #gibt das gesamtgewicht aller tiere zurück
+    def gesamtgewicht(self) -> float:
+        for i in range(len(self.tiere)):
+            gesamtgewicht_tiere = self.tiere[i].gewicht
+            return gesamtgewicht_tiere
+
+    #gibt die anzahl aller tiere zurück
+    def anzahl_tierart(self, tierart: str) -> int:
+        anzahl_tierart = 0
+        for tier in self.tiere:
+            if self.tier == tierart:
+                anzahl_tierart += 1
+        return
+
+
+    #Gibt das schwerste tier an
+    def schwerste(self) -> Tier:
+        for i in range(len(self.tiere)):
+            for u in range(len(self.tiere)):
+                if self.tiere[i].gewicht < self.tiere[u].gewicht:
+                    break
+        
+        return self.tiere[i]
+
+            
+
+    #gibt das gesamtgewicht aller tiere zurück
+
 
 tier1 = Tier("Löwe", ["Zebra", "Gazelle", "Gnu"], [], 150.0, 150, "gelb")
 tier2 = Tier("Zebra", [], ["Löwe"], 300.0, 210, "schwarz-weiß")
 tier3 = Tier("Gnu", [], ["Löwe"], 200.0, 190, "schwarz")
 
-print(tier1.tierart)
-print(tier2.farbe)
-print(tier3.fressen())
-print(tier1.füttern(2.0))
-print(tier3.wachsen(30))
+# print(tier1.tierart)
+# print(tier2.farbe)
+# print(tier3.fressen())
+# print(tier1.füttern(2.0))
+# tier3.wachsen(30)
+# print(tier3.gewicht)
+tier4 = tier3.junges()
+print(tier4)
+safaripark = Safaripark('hahahahahh', [tier1, tier2, tier3], 100.00)
+print(schwerste(safaripark))
