84d04c3cb3482e5b712d186c355df22bfe10cec6
pge1414 <101663790+pge1414@users.noreply.github.com>
2022-09-13 18:59:07 +0200

    ss

--- Tic-Tac-Toe/spieler_computer_schlau.py
+++ Tic-Tac-Toe/spieler_computer_schlau.py
@@ -1,3 +1,4 @@
+
 import random
 class Spieler_Computer_Schlau:
     def __init__(self,name):
@@ -19,14 +20,59 @@ class Spieler_Computer_Schlau:
         return [freies_feld for freies_feld in spielfeld if freies_feld not in ["x", "o"]]
 
     def __bewertung(self, spielfeld,symbol):
-        if symbol == self.symbol:
-            if self.__gewinnprüfung(spielfeld):
-                return -(len(self.__freie_felder(spielfeld))+1)
-            if len(self.__freie_felder(spielfeld)) == 0:
-                return 0
-            max = -1
-            for freies_feld in self.__freie_felder(spielfeld):
-                spielfeld[int(freies_feld) - 1] = self.symbol
-                bewertung = self.__bewertung(spielfeld, symbol_gegner)
-                if bewertung > max:
-                    max = bewertung
+        # if symbol == self.symbol:
+        #     if self.__gewinnprüfung(spielfeld):
+        #         return -(len(self.__freie_felder(spielfeld))+1)
+        #     if len(self.__freie_felder(spielfeld)) == 0:
+        #         for freies_feld in self.freie_felder(spielfeld):
+        #             symbol == self.symbol
+        #             if self.__gewinnprüfung(spielfeld):
+        #                 return -(len(self.__freie_felder(spielfeld))+1)
+        #             if len(self.__freie_felder(spielfeld)) == 0:
+        #                 for freies_feld in self.freie_felder(spielfeld):
+        #                     symbol == self.symbol_gegner
+        #                     if self.gewinnprüfung(spielfeld):
+        #                         return -(len(self.__freie_felder(spielfeld))+1)
+        #                     if len(self.freie_felder(spielfeld))==0:
+        #                         return self.symbol == random.choice(self.freie_felder(spielfeld))
+        # for freies_feld in self.freie_felder(spielfeld):
+        #     freies_feld = self.symbol
+        #     bewertung = 0
+        #     if self.gewinnprüfung(spielfeld):
+        #         return {bewertung: freies_feld}
+        #     if not self.gewinnprüfung(spielfeld):
+        #         bewertung += 1
+        #         freies_feld = self.symbol_gegner
+        #         for freies_feld in self.freie_felder(spielfeld):
+        #             if self.gewinnprüfung(spielfeld):
+        #                 return {bewertung: freies_feld}
+        #             if not self.gewinnprüfung(spielfeld):
+        #                 bewertung += 1
+        #                 freies_feld = self.symbol
+        #                 for freies_feld in self.freie_felder(spielfeld):
+        #                     if self.gewinnprüfung(spielfeld):
+        #                         return {bewertung: freies_feld}
+        #                     if not self.gewinnprüfung(spielfeld):
+        #                         break
+
+            
+                                
+                
+
+    def minimax(self, spielfeld):
+            # max = -10
+            # bestes_feld = None
+            # for freies_feld in self.__freie_felder(spielfeld):
+            #     spielfeld[int(freies_feld) - 1] = self.symbol
+            #     bewertung = self.__bewertung(spielfeld, self.symbol_gegner)
+            #     if bewertung > max:
+            #         max = bewertung
+            #         bestes_feld = freies_feld
+            #     spielfeld[int(freies_feld)-1] = freies_feld
+            bestes_feld = self.symbol
+            return bestes_feld
+
+    def zug(self, spielfeld):
+        self.symbol_gegner = "x" if self.symbol == "o" else "o"
+        return int(self.minimax(spielfeld))-1
+
